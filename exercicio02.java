dd6432f2934698d5655ec67edf6f03e27e6713a4
Juliano Cabral <juliano.ifrank@gmail.com>
2025-09-27 10:27:39 -0300

    exercicio java

---
+++ exercicio02.java
@@ -0,0 +1,20 @@
+
+public class exercicio02 {
+    public static void main(String[] args) {
+        String [] nomes = new String[6];
+
+        nomes[0] ="Ana";
+        nomes[1] ="Bruno";
+        nomes[2] ="Carla";
+        nomes[3] ="Daniel";
+        nomes[4] ="Erica";
+        nomes[5] = "fabio";
+
+        int[] notas = {55, 80, 90, 100, 115};
+
+        System.out.println("quantidade de nomes " + nomes.length);
+        System.out.println(nomes[5]);
+
+
+    }
+}
